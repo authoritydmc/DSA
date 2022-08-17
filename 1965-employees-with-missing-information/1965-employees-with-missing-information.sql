f08cddfdcb1a28fbf0863d6f784c267512e03e9f
Raj Dubey <raj.dubey@outlook.in>
2022-08-17 23:21:53 +0530

    Time: 541 ms (68.49%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,4 +1,14 @@
-select employee_id from (SELECT employee_id FROM employees e
-where employee_id not in (select employee_id from salaries)
-UNION
-SELECT employee_id FROM salaries where  employee_id not in (select employee_id from employees)) as t order by t.employee_id asc;
+# select employee_id from (SELECT employee_id FROM employees e
+# where employee_id not in (select employee_id from salaries)
+# UNION
+# SELECT employee_id FROM salaries where  employee_id not in (select employee_id from employees)) as t order by t.employee_id asc;
+
+
+SELECT T.employee_id
+FROM  
+  (SELECT * FROM Employees LEFT JOIN Salaries USING(employee_id)
+   UNION 
+   SELECT * FROM Employees RIGHT JOIN Salaries USING(employee_id))
+AS T
+WHERE T.salary IS NULL OR T.name IS NULL
+ORDER BY employee_id;
