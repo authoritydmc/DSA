d179e0db001be969bd5d527eba1010f6d353684f
Raj Dubey <raj.dubey@outlook.in>
2024-11-24 11:43:46 +0530

    Time: 36 ms (18.86%), Space: 45.7 MB (12.9%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,8 +1,8 @@
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n + 1];
-    Arrays.fill(dp,Integer.MAX_VALUE);
+        int[] dp = new int[n ];
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
 
         for (int i = 0; i < n; i++) {
@@ -10,10 +10,11 @@ class Solution {
             int min_jumps = Integer.MAX_VALUE;
             int allowed_jumps = nums[i];
             // System.out.println("Index: " + i + ", Allowed jumps: " + allowed_jumps);
-     
+
             for (int j = 1; j <= allowed_jumps; j++) {
                 if (i + j < n) {
-                    // System.out.println("Updating dp[" + (i + j) + "] from " + dp[i + j] + " to " + Math.min(dp[i + j], 1 + dp[i]));
+                    // System.out.println("Updating dp[" + (i + j) + "] from " + dp[i + j] + " to "
+                    // + Math.min(dp[i + j], 1 + dp[i]));
                     dp[i + j] = Math.min(dp[i + j], 1 + dp[i]);
                 }
             }
