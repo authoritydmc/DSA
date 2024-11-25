12e02a8b69a655651a1682411e29ca1b9d8cdace
Raj Dubey <raj.dubey@outlook.in>
2024-11-25 23:30:50 +0530

    Time: 1 ms (99%), Space: 40.8 MB (85.67%) - LeetHub

---
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int stone : stones) {
+            pq.add(stone);
+        }
+        int res=0;
+        while (true) {
+              if(pq.size()<=1)
+              break;
+                int y=pq.poll();
+               
+                int x=pq.poll();
+                if(x!=y)
+                pq.add(y-x);
+        }
+        if(pq.size()!=0)
+        res=pq.poll();
+        return res;
+    }
+}
