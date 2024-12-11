b18ca08830072cc8dbd28d84a966eed4055ef729
Raj Dubey <raj.dubey@outlook.in>
2024-12-11 20:57:13 +0530

    Time: 40 ms (53.3%), Space: 60.5 MB (28.22%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,32 +1,18 @@
 class Solution {
-    private int binarySearch(int []nums,int start,int target)
-    {
-        int l=start;
-        int r=nums.length-1;
-        int res=r;
-        while(l<=r)
-        {
-            int mid=l+(r-l)/2;
-            if(nums[mid]<=target)
-            {
-                res=mid;
-                //we have found a valid index we can push right to check if more furthest valid index
-                l=mid+1;
-            }else
-            r=mid-1;
-
-        }
-        return res;
-    }
     public int maximumBeauty(int[] nums, int k) {
         Arrays.sort(nums);
+        int i = 0;
+        int j = 0;
+        int n = nums.length;
         int max=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            int maxRange=nums[i]+2*k;
-            int farthestGoodIndex=binarySearch(nums,i,maxRange);
-            max=Math.max(max,farthestGoodIndex-i+1);
+        while (j < n) {
+            int maxRange = nums[i] + 2 * k;
+            while (j<n && nums[j] <= maxRange) {
+                j++;
+                max = Math.max(max, j - i );
+            }
 
+            i++;
         }
 
         return max;
