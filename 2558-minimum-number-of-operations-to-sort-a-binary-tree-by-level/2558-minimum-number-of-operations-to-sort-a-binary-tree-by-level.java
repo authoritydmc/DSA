fe2e7a3b738f87add0c82f2f6d385d59fc956f3f
Raj Dubey <raj.dubey@outlook.in>
2024-12-23 17:45:54 +0530

    Time: 49 ms (79.45%), Space: 59.5 MB (95.89%) - LeetHub

--- 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.java
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.java
@@ -16,48 +16,32 @@
  */
 class Solution {
     private int makeSorted(int[] arr) {
-      int ans = 0;
-      int N=arr.length;
-        int[] temp = Arrays.copyOfRange(arr, 0, N);
+        int swaps = 0;
+        int[] target = arr.clone();
+        Arrays.sort(target);
 
-        // Hashmap which stores the
-        // indexes of the input array
-        HashMap<Integer, Integer> h
-            = new HashMap<Integer, Integer>();
-
-        Arrays.sort(temp);
-        for (int i = 0; i < N; i++) {
-            h.put(arr[i], i);
+        // Map to track current positions of values
+        Map<Integer, Integer> pos = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            pos.put(arr[i], i);
         }
-        for (int i = 0; i < N; i++) {
 
-            // This is checking whether
-            // the current element is
-            // at the right place or not
-            if (arr[i] != temp[i]) {
-                ans++;
-                int init = arr[i];
+        // For each position, swap until correct value is placed
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != target[i]) {
+                swaps++;
 
-                // If not, swap this element
-                // with the index of the
-                // element which should come here
-
-                swap(arr, i, h.get(temp[i]));
-
-                // Update the indexes in
-                // the hashmap accordingly
-                h.put(init, h.get(temp[i]));
-                h.put(temp[i], i);
+                // Update position of swapped values
+                int curPos = pos.get(target[i]);
+                pos.put(arr[i], curPos);
+                arr[curPos] = arr[i];
             }
         }
-        return ans;
+        return swaps;
     }
-    public void swap(int[] arr, int i, int j)
-    {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+
+ 
+
     public int minimumOperations(TreeNode root) {
         int count = 0;
 
