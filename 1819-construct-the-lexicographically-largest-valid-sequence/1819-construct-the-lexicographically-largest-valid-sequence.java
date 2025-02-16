25218e7994a36d38a2077c270da0e519027a3d81
Raj Dubey <raj.dubey@outlook.in>
2025-02-16 19:14:59 +0530

    Time: 2 ms (19.71%), Space: 41.5 MB (8.76%) - LeetHub

--- 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.java
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.java
@@ -1,50 +1,49 @@
 class Solution {
 
-    private boolean backtrack(int n, int i, int[] res, boolean[] used) {
-        // Base case: if we reach the end of the array
-        if (i >= res.length)
+    private boolean backtrack(int n, int i, int[] res, Set<Integer> used) {
+         if (i >= res.length)
             return true;
-    
-        if (res[i] != -1)
+         if (res[i] != -1)
             return backtrack(n, i + 1, res, used);
 
-        // Try placing numbers from n to 1
         for (int j = n; j >= 1; j--) {
-            if (used[j]) continue;
-            
-        
-            used[j] = true;
+
+            if (used.contains(j))
+                continue;
+
+            used.add(j);
             res[i] = j;
-            
-            if (j == 1) { 
-                if (backtrack(n, i + 1, res, used)) return true;
+
+            if (j == 1) {
+                if (backtrack(n, i + 1, res, used) == true)
+                    return true;
             } else {
                 int next_ind = i + j;
                 if (next_ind < res.length && res[next_ind] == -1) {
                     res[next_ind] = j;
 
-                    if (backtrack(n, i + 1, res, used)) return true;
+                    if (backtrack(n, i + 1, res, used) == true)
+                        return true;
 
-                   
                     res[next_ind] = -1;
                 }
+
             }
 
-           
+            used.remove(j);
             res[i] = -1;
-            used[j] = false;
+
         }
+
         return false;
     }
 
     public int[] constructDistancedSequence(int n) {
         int[] res = new int[2 * n - 1];
-        Arrays.fill(res, -1);  
-        boolean[] used = new boolean[n + 1];  
-        
+        Arrays.fill(res,-1);
+        Set<Integer> used = new HashSet<>();
         backtrack(n, 0, res, used);
+
         return res;
     }
-
-   
 }
