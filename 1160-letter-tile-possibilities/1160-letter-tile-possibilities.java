a32efc889bfcd580bbf5f0da3d44f494a1389480
Raj Dubey <raj.dubey@outlook.in>
2025-02-17 14:25:38 +0530

    Time: 22 ms (39.6%), Space: 45.6 MB (26.01%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
@@ -3,22 +3,23 @@ import java.util.Set;
 
 class Solution {
 
-    private void backtrack(String s, String cur, boolean[] used, Set<String> uniqueWords) {
+    private void backtrack(String s, StringBuilder cur, boolean[] used, Set<String> uniqueWords) {
 
         for (int i = 0; i < s.length(); i++) {
             if (used[i])
                 continue; // Skip used characters
 
             used[i] = true;
-            String newWord = cur + s.charAt(i);
+            cur.append(s.charAt(i));
 
-            uniqueWords.add(newWord);
+            uniqueWords.add(cur.toString());
 
             // Explore further
-            backtrack(s, newWord, used, uniqueWords);
+            backtrack(s, cur, used, uniqueWords);
 
             // Backtrack
             used[i] = false;
+            cur.deleteCharAt(cur.length() - 1);
         }
 
         return;
@@ -27,7 +28,7 @@ class Solution {
     public int numTilePossibilities(String tiles) {
         boolean[] used = new boolean[tiles.length()];
         Set<String> uniqueWords = new HashSet<>(); // To track unique sequences
-        backtrack(tiles, "", used, uniqueWords);
+        backtrack(tiles, new StringBuilder(), used, uniqueWords);
 
         return uniqueWords.size();
     }
