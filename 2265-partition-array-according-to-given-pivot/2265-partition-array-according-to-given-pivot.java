351b2a99be96dedc8b1a4908ca1f931622a00a52
Raj Dubey <raj.dubey@outlook.in>
2025-03-03 14:09:25 +0530

    Time: 5 ms (85.39%), Space: 68.6 MB (5.53%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,31 +1,32 @@
 class Solution {
-
     public int[] pivotArray(int[] nums, int pivot) {
-        int cnt = 0;
-
-        Queue<Integer> lq = new LinkedList<>();
-        Queue<Integer> rq = new LinkedList<>();
-
-        for(int n:nums)
-        {
-            if(n<pivot)
-            lq.offer(n);
-            else if(n>pivot)
-            rq.offer(n);
-            else
-            cnt++;
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        int left = 0;
+        int right = n - 1;
+        
+        // First, place all the elements less than pivot
+        for (int num : nums) {
+            if (num < pivot) {
+                result[left++] = num;
+            }
         }
-
-        int start = 0;
-
-        while (!lq.isEmpty())
-            nums[start++] = lq.poll();
-
-        while (cnt-- > 0)
-            nums[start++] = pivot;
-
-        while (!rq.isEmpty())
-            nums[start++] = rq.poll();
-        return nums;
+        
+        // Then, place all the elements equal to pivot
+        for (int num : nums) {
+            if (num == pivot) {
+                result[left++] = num;
+            }
+        }
+        
+        // Finally, place all the elements greater than pivot
+        for (int num : nums) {
+            if (num > pivot) {
+                result[left++] = num;
+            }
+        }
+        
+        return result;
     }
 }
