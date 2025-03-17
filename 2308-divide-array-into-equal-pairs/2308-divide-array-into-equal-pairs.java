8630116fc3d9f76a0afde1a3ee11c1b24c9ccea9
Raj Dubey <raj.dubey@outlook.in>
2025-03-17 21:08:38 +0530

    Time: 1 ms (100%), Space: 44.2 MB (94.91%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -1,15 +1,11 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public boolean divideArray(int[] nums) {
-        Map<Integer, Integer> countMap = new HashMap<>();
+        int[] counts = new int[501];
         for (int num : nums) {
-            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            counts[num]++;
         }
         
-        // Check if all counts are even
-        for (int count : countMap.values()) {
+        for (int count : counts) {
             if (count % 2 != 0) {
                 return false;
             }
